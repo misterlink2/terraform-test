e64e6752430cf43c66dc34695c19a5c848ef9e0e
misterlink2 <austinbasala@ymail.com>
2022-07-23 10:34:37 -0500

    adding backend bucket and test bucket

---
+++ bucket.tf
@@ -0,0 +1,26 @@
+resource "aws_s3_bucket" "web-bucket" {
+    bucket = "test-infra-bucket-austin-1"
+}
+
+resource "aws_s3_bucket_policy" "s3-get-policy"{
+  bucket = aws_s3_bucket.test-tf-bucket.id
+  policy = data.aws_iam_policy_document.get-object-policy.json
+}
+
+data "aws_iam_policy_document" "get-object-policy" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["460627244109"]
+    }
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      aws_s3_bucket.test-tf-bucket.arn,
+      "${aws_s3_bucket.test-tf-bucket.arn}/*",
+    ]
+  }
+}
